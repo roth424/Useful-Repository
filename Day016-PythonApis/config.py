ee075a3698a476a3855aeb9285d4c44409d8ff47
Kingaby <skingaby@deloitte.com>
2019-06-29 09:00:29 -0500

    Checking in last minute changes

--- Day016-PythonApis/config.py
+++ Day016-PythonApis/config.py
@@ -1,2 +1,5 @@
-nyt_api_key = "YOUR KEY HERE!"
-omdb_api_key = "YOUR KEY HERE"
+nyt_api_key = "zPz9YTRwvv7KYMLQUXKsgNX2iGciSCAd"
+nyt_secret = "4LRgHVplcskN7BUL"
+omdb_api_key = "7361ca1c"
+census_key = "90a3b4413de14a5a52626999ba58589f7a37f90d"
+weather_key = "aca59d3a1814cab4d29c2b6ca6c4c22c"
